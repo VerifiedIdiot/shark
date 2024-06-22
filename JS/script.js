ab8c7fde011eec2f2f8fe87cade66652d007eabd
moonjiye <moonjiye@gmail.com>
2024-06-22 21:48:57 +0900

    shark

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    var menuIcon = document.getElementById("menu-icon");
+    var menu = document.getElementById("menu");
+
+    menuIcon.classList.toggle("active");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
